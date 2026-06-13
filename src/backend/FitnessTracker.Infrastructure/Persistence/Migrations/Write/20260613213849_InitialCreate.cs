87c6e5d14256f3eff0798ddf13dfa652b0625eb2
Danylo Bodnar <danylobodnar96@gmail.com>
2026-06-13 23:40:52 +0200

    refactor: split migrations into Write/Read subfolders
    - Move Write migrations to Persistence/Migrations/Write/
    - Create Read migrations in Persistence/Migrations/Read/
    - Register ReadDbContext in DI
    - AddReadModels migration for read model tables

--- src/backend/FitnessTracker.Infrastructure/Persistence/Migrations/Write/20260613213849_InitialCreate.cs
+++ src/backend/FitnessTracker.Infrastructure/Persistence/Migrations/Write/20260613213849_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace FitnessTracker.Infrastructure.Migrations
+namespace FitnessTracker.Infrastructure.Persistence.Migrations.Write
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
