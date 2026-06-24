152cb4eddec6b00fe26205299cbd5ec2aa6e1110
Danylo Bodnar <danylobodnar96@gmail.com>
2026-06-24 15:22:37 +0200

    chore: increase access token expiry from 3 to 15 minutes

--- src/backend/FitnessTracker.Application/Common/Options/JwtOptions.cs
+++ src/backend/FitnessTracker.Application/Common/Options/JwtOptions.cs
@@ -7,5 +7,5 @@ public class JwtOptions
     public string Secret { get; set; } = default!;
     public string Issuer { get; set; } = default!;
     public string Audience { get; set; } = default!;
-    public int ExpiryMinutes { get; set; } = 3;
+    public int ExpiryMinutes { get; set; } = 15;
 }
