8500b064859a7d18c3eb320023c509d3b218842d
Danylo Bodnar <danylobodnar96@gmail.com>
2026-06-26 10:40:35 +0200

    refactor: update ProgramExercise to use primitive types for TargetSets and TargetReps

--- src/backend/FitnessTracker.Domain/Entities/ProgramExercise.cs
+++ src/backend/FitnessTracker.Domain/Entities/ProgramExercise.cs
@@ -7,13 +7,13 @@ public class ProgramExercise
     public Guid Id { get; private set; }
     public Guid ExerciseId { get; private set; }
     public ExerciseName ExerciseName { get; private set; } = null!;
-    public Sets TargetSets { get; private set; } = null!;
-    public Repetitions TargetReps { get; private set; } = null!;
+    public int TargetSets { get; private set; }
+    public int TargetReps { get; private set; }
     public int Order { get; private set; }
 
     private ProgramExercise() { }
 
-    public ProgramExercise(Guid exerciseId, ExerciseName exerciseName, Sets targetSets, Repetitions targetReps, int order)
+    public ProgramExercise(Guid exerciseId, ExerciseName exerciseName, int targetSets, int targetReps, int order)
     {
         Id = Guid.NewGuid();
         ExerciseId = exerciseId;
