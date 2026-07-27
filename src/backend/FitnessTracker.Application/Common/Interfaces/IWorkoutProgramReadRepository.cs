e780e8563e2ac76b6634cd4dfbbcfd74a46cefe2
Danylo Bodnar <danylobodnar96@gmail.com>
2026-07-27 22:55:25 +0200

    Fix superset uneven set counts, add GetByIdAsync, remove null-forgiving operator

--- src/backend/FitnessTracker.Application/Common/Interfaces/IWorkoutProgramReadRepository.cs
+++ src/backend/FitnessTracker.Application/Common/Interfaces/IWorkoutProgramReadRepository.cs
@@ -5,4 +5,5 @@ namespace FitnessTracker.Application.Common.Interfaces;
 public interface IWorkoutProgramReadRepository
 {
     Task<List<WorkoutProgramDto>> GetByUserAsync(Guid userId, CancellationToken ct = default);
+    Task<WorkoutProgramDto?> GetByIdAsync(Guid programId, Guid userId, CancellationToken ct = default);
 }
